c40b3771a6892b11444ba629e1843810f2c848b5
nb510 <nikita.burak.dev@gmail.com>
2025-09-09 11:31:44 +0300

    add ID to BeerOrderLineDto

--- src/main/java/guru/springframework/spring6restmvcapi/BeerOrderLineDto.java
+++ src/main/java/guru/springframework/spring6restmvcapi/BeerOrderLineDto.java
@@ -3,9 +3,12 @@ package guru.springframework.spring6restmvcapi;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 @Builder
 public class BeerOrderLineDto {
+    private UUID id;
     private BeerDto beer;
     private Integer orderQuantity;
     private Integer quantityAllocated;
