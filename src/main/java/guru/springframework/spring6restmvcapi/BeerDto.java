ffc471c6a1551ef30b0e8cdd3c868a4f6b1d8143
nb510 <nikita.burak.dev@gmail.com>
2025-09-11 16:25:11 +0300

    fix Kafka deserialization

--- src/main/java/guru/springframework/spring6restmvcapi/BeerDto.java
+++ src/main/java/guru/springframework/spring6restmvcapi/BeerDto.java
@@ -3,8 +3,10 @@ package guru.springframework.spring6restmvcapi;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -12,6 +14,8 @@ import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BeerDto {
 
     private UUID id;
--- src/main/java/guru/springframework/spring6restmvcapi/BeerOrderLineDto.java
+++ src/main/java/guru/springframework/spring6restmvcapi/BeerOrderLineDto.java
@@ -1,12 +1,16 @@
 package guru.springframework.spring6restmvcapi;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BeerOrderLineDto {
     private UUID id;
     private BeerDto beer;
