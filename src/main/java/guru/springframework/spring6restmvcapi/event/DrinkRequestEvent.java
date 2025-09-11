0fb078a890dc9288dcd4519d82a1e0e5d31415a9
nb510 <nikita.burak.dev@gmail.com>
2025-09-11 16:25:24 +0300

    add events

---
+++ src/main/java/guru/springframework/spring6restmvcapi/event/DrinkRequestEvent.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring6restmvcapi.event;
+
+import guru.springframework.spring6restmvcapi.BeerOrderLineDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DrinkRequestEvent {
+
+    private BeerOrderLineDto beerOrderLineDTO;
+}
